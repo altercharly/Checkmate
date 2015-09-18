54f43eb1fa9e6201803f53bbcabd63f92cfb650a
jimenasantiago <jimmlala@hotmail.com>
2015-09-18 20:11:42 -0300

    agregue FactoryConexion

--- Checkmate/src/capadata/FactoryConexion.java
+++ Checkmate/src/capadata/FactoryConexion.java
@@ -1,7 +1,65 @@
 package capadata;
 
-import java.sql.*;
-
 public class FactoryConexion {
-  // crear el factoria para la coneccion a la DB//
+
+
+	private String dbDriver="com.mysql.jdbc.Driver";
+	private String host="localhost";
+	private String port="3306";
+	private String user="usuariojava";
+	private String pass="jime";
+	private String db="java";
+	
+	private Connection conn;
+	private int cantCon;
+	
+	private FactoryConexion(){
+		try {
+			Class.forName(dbDriver);
+			conn=null;
+			cantCon=0;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static FactoryConexion instancia;
+	
+	public static FactoryConexion getInstancia(){
+		if (instancia==null){
+			instancia = new FactoryConexion();
+		}
+		return instancia;
+	}
+	
+	
+	
+	public Connection getConn(){
+		try {
+			if(conn==null || conn.isClosed()){
+				conn = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db+"?user="+user+"&password="+pass);
+				cantCon++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	
+	public void releaseConn(){
+		try {
+			cantCon--;
+			if(cantCon==0){
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
+
+
